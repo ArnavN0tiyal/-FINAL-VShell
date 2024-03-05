22f17c3b154d459a43f7b2b1b5a7a781b0aa305b
ConditionalCode <125085107+ArnavN0tiyal@users.noreply.github.com>
2024-03-05 17:59:39 +0530

    Update Commands.java
    new commands

--- VShell/Commands.java
+++ VShell/Commands.java
@@ -1,6 +1,8 @@
 package VShell;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,22 +13,50 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Commands {
-    static String cmd;
+    static String usercmd;
     static final Runtime run = Runtime.getRuntime();
     static Process proc;
     static int exit = 1;
     static final Scanner sc = new Scanner(System.in);
 
+    public static void cmd() throws IOException {
+        String processedCmd = usercmd.replace("cmd", "").trim();
+        String[] cmdBuild = { "cmd", "/C", processedCmd };
+        ProcessBuilder builder = new ProcessBuilder(cmdBuild);
+
+        Process process = builder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
+    public static void powershell() throws IOException {
+        String processedPS = usercmd.replace("ps", "").trim();
+        String[] psBuild = { "powershell", "/C", processedPS };
+        ProcessBuilder builder = new ProcessBuilder(psBuild);
+
+        Process process = builder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
     public static void help() {
-        System.out.println("About:\nWelcome to VShell (Virtual Shell)! This is a custom \"shell\" fully made in java by ArnavN0tiyal. Commands in this \"shell\" are - print, time, eval, run, ascii, colour, shutdown and exit.\nHow to use commands:\nCommands are pretty straightforward to use.\nFirst is print, which prints to the console. To use this command, first type \"print\" then the string, like - [print hello world].\nSecond is time, which *obviously* shows the time.\nThird is eval, which evalutes statements like - [eval 2 + 2] Output - 4.\nFourth is run, which runs programs like - [run cmd].\nFifth is ascii, which shows the ascii value of characters like - [ascii I].\nSixth is shutdown, which shuts down your computer in certain ways like if I want to restart, do - [shutdown /r] etc, bascially the command prompt shutdown method.\nSeventh is colour, which changes the text colour like - [colour red].\nEighth is clear, which clears the console.\nNinth is dirfiles, which shows the files in the presetted directory, change the directory yourself.\nLast but not the least, tenth is shver, which shows the version of the VShell.");
+        System.out.println("About:\n\nWelcome to VShell (Virtual Shell)! This is a custom \"shell\" fully made in java by ArnavN0tiyal. Commands in this \"shell\" are - print, time, eval, run, ascii, colour, shutdown and exit.\n\nHow to use commands:\n\nCommands are pretty straightforward to use.\nFirst is print, which prints to the console. To use this command, first type \"print\" then the string, like - [print hello world].\nSecond is time, which *obviously* shows the time.\nThird is eval, which evalutes statements like - [eval 2 + 2] Output - 4.\nFourth is run, which runs programs like - [run cmd].\nFifth is ascii, which shows the ascii value of characters like - [ascii I].\nSixth is shutdown, which shuts down your computer in certain ways like if I want to restart, do - [shutdown /r] etc, bascially the command prompt shutdown method.\nSeventh is colour, which changes the text colour like - [colour red].\nEighth is clear, which clears the console.\nNinth is dirfiles, which shows the files in the presetted directory, change the directory yourself.\nTenth is shver, which shows the version of the VShell.\nEleventh is cmd, which runs cmd commands, like - [cmd echo Hello, world!].\nTwelfeth is ps, which runs powershell commands, like - [ps Write-Output Hello,world!].");
     }
 
     public static void shellversion() {
-        System.out.println("Version: 1.1\nRelease: 24/3/3");
+        System.out.println("Version: 1.2\nRelease: 24/3/5");
     }
 
     public static void print() {
-        String processed = cmd.replace("print", "").trim();
+        String processed = usercmd.replace("print", "").trim();
         System.out.println(processed);
     }
 
@@ -36,7 +66,7 @@ public class Commands {
     }
 
     public static void eval() {
-        String processed = cmd.replace("eval", "").trim();
+        String processed = usercmd.replace("eval", "").trim();
         String[] tokens = processed.split("\\s");
         try {
             double num1 = Double.parseDouble(tokens[0]);
@@ -62,51 +92,52 @@ public class Commands {
     }
 
     public static void run() throws IOException {
-        if (cmd.endsWith("edge")) {
+        if (usercmd.endsWith("edge")) {
             proc = run.exec(new String[] { "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe" });
-        } else if (cmd.endsWith("cmd")) {
+        } else if (usercmd.endsWith("cmd")) {
             proc = run.exec(new String[] { "cmd", "/C", "start" });
-        } else if (cmd.endsWith("code")) {
+        } else if (usercmd.endsWith("code")) {
             proc = run.exec(new String[] { "cmd", "/C", "code" });
-        } else if (cmd.endsWith("arc")) {
+        } else if (usercmd.endsWith("arc")) {
             proc = run.exec(new String[] { "cmd", "/C", "arc" });
-        } else if (cmd.endsWith("cursor")) {
+        } else if (usercmd.endsWith("cursor")) {
             proc = run.exec(new String[] { "cmd", "/C", "cursor" });
-        } else if (cmd.endsWith("vi")) {
+        } else if (usercmd.endsWith("vi")) {
             proc = run.exec(new String[] { "cmd", "/C", "start", "vi" });
-        } else if (cmd.endsWith("vim")) {
+        } else if (usercmd.endsWith("vim")) {
             proc = run.exec(new String[] { "cmd", "/C", "start", "vim" });
-        } else if (cmd.endsWith("emacs")) {
+        } else if (usercmd.endsWith("emacs")) {
             proc = run.exec(new String[] { "cmd", "/C", "start", "emacs" });
-        } else if (cmd.endsWith("nano")) {
+        } else if (usercmd.endsWith("nano")) {
             proc = run.exec(new String[] { "cmde", "/C", "start", "nano" });
-        } else if (cmd.endsWith("powershell")) {
+        } else if (usercmd.endsWith("powershell")) {
             proc = run.exec(new String[] { "cmd", "/C", "start", "powershell" });
-        } else if (cmd.endsWith("powershell ise")) {
+        } else if (usercmd.endsWith("powershell ise")) {
             proc = run.exec(new String[] { "cmd", "/C", "powershell ise" });
-        } else  if (cmd.endsWith("calculator")) {
-            proc = run.exec(new String[] { "cmd", "/C", "start", "calc"});
+        } else if (usercmd.endsWith("calculator")) {
+            proc = run.exec(new String[] { "cmd", "/C", "start", "calc" });
         } else {
-            System.out.println("ERROR! Program not recognized, use [edge, cmd, code, arc, cursor, vi, vim, emacs, nano, powershell, powershell ise, calculator]");
+            System.out.println(
+                    "ERROR! Program not recognized, use [edge, cmd, code, arc, cursor, vi, vim, emacs, nano, powershell, powershell ise, calculator]");
         }
     }
 
     public static void shutdown() throws IOException {
-        if (cmd.endsWith("i")) {
+        if (usercmd.endsWith("i")) {
             proc = run.exec(new String[] { "cmd", "/C", "Start", "shutdown", "/i" });
-        } else if (cmd.endsWith("l")) {
+        } else if (usercmd.endsWith("l")) {
             proc = run.exec(new String[] { "cmd", "/C", "Start", "shutdown", "/l" });
-        } else if (cmd.endsWith("s")) {
+        } else if (usercmd.endsWith("s")) {
             proc = run.exec(new String[] { "cmd", "/C", "Start", "shutdown", "/s" });
-        } else if (cmd.endsWith("r")) {
+        } else if (usercmd.endsWith("r")) {
             proc = run.exec(new String[] { "cmd", "/C", "Start", "shutdown", "/r" });
-        } else if (cmd.endsWith("g")) {
+        } else if (usercmd.endsWith("g")) {
             proc = run.exec(new String[] { "cmd", "/C", "Start", "shutdown", "/g" });
-        } else if (cmd.endsWith("a")) {
+        } else if (usercmd.endsWith("a")) {
             proc = run.exec(new String[] { "cmd", "/C", "Start", "shutdown", "/a" });
-        } else if (cmd.endsWith("p")) {
+        } else if (usercmd.endsWith("p")) {
             proc = run.exec(new String[] { "cmd", "/C", "Start", "shutdown", "/p" });
-        } else if (cmd.endsWith("h")) {
+        } else if (usercmd.endsWith("h")) {
             proc = run.exec(new String[] { "cmd", "/C", "Start", "shutdown", "/h" });
         } else {
             System.out.println("ERROR! Shutdown type not recognized, use [/i, /l, /s, /r, /g, /a, /p, /h]");
@@ -130,26 +161,25 @@ public class Commands {
     }
 
     public static void colour() {
-        if (cmd.endsWith("reset")) {
+        if (usercmd.endsWith("reset")) {
             System.out.print("\033[0m");
-        } else if (cmd.endsWith("black")) {
+        } else if (usercmd.endsWith("black")) {
             System.out.print("\033[0;30m");
-        } else if (cmd.endsWith("red")) {
+        } else if (usercmd.endsWith("red")) {
             System.out.print("\033[0;31m");
-        } else if (cmd.endsWith("orange")) {
+        } else if (usercmd.endsWith("orange")) {
             System.out.print("\033[0;33m");
-        } else if (cmd.endsWith("blue")) {
+        } else if (usercmd.endsWith("blue")) {
             System.out.print("\033[0;34m");
-        } else if (cmd.endsWith("green")) {
+        } else if (usercmd.endsWith("green")) {
             System.out.print("\033[0;92m");
         } else {
             System.out.println("ERROR, Colour not recognized, use [reset, black, red, orange, blue, green]");
         }
     }
-    
 
     public static void ascii() throws IOException {
-        char valueinput = cmd.charAt(6);
+        char valueinput = usercmd.charAt(6);
         int asciivalue = valueinput;
         System.out.println(asciivalue);
     }
